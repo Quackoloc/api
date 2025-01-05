94da681c5f5e19cbd634783a688bc43d4772cfe4
Lapanxd <morgan.dumas@outlook.fr>
2025-01-05 15:15:38 +0100

    CL-17: ETQU, je peux m'inscrire et me connecter

---
+++ src/users/exceptions/users-exception.ts
@@ -0,0 +1,31 @@
+import { AppException } from '../../app-exception';
+import { HttpStatus } from '@nestjs/common';
+
+export class UsersException extends AppException {}
+
+export class UserNotFoundException extends UsersException {
+  public static idNotFound(id: string): UserNotFoundException {
+    return new UserNotFoundException(
+      `User with id: ${id} not found.`,
+      HttpStatus.NOT_FOUND,
+    );
+  }
+
+  public static emailNotFound(email: string): UserNotFoundException {
+    return new UserNotFoundException(
+      `User with email: ${email} not found.`,
+      HttpStatus.NOT_FOUND,
+    );
+  }
+}
+
+export class UserCannotBeCreatedException extends UsersException {
+  public static emailAlreadyExists(
+    email: string,
+  ): UserCannotBeCreatedException {
+    return new UserCannotBeCreatedException(
+      `User with email: ${email} already exists.`,
+      HttpStatus.CONFLICT,
+    );
+  }
+}
