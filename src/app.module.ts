e42fdd3ca4c99eb1b51be082325ceb28e7b300c4
Lapanxd <morgan.dumas@outlook.fr>
2025-01-05 15:15:38 +0100

    feat(auth): [QUAC-43] Créer les routes de register/login sur l'API

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { localConf } from './config/database.config';
+import { APP_FILTER } from '@nestjs/core';
+import { ExceptionHandler } from '@nestjs/core/errors/exception-handler';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    AuthModule,
+    UsersModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: localConf,
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
+  providers: [{ provide: APP_FILTER, useClass: ExceptionHandler }],
 })
 export class AppModule {}
