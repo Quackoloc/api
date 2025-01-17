07888f37bac2998098b815c6a2f029ce2b0c1792
Lapanxd <morgan.dumas@outlook.fr>
2025-01-17 18:36:44 +0100

    feat: add pending users service

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -4,11 +4,12 @@ import { User } from './entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { PendingUser } from './entities/pending-user.entity';
+import { PendingUsersService } from './pending-users.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User, PendingUser])],
   controllers: [UsersController],
-  providers: [UsersService],
-  exports: [UsersService],
+  providers: [UsersService, PendingUsersService],
+  exports: [UsersService, PendingUsersService],
 })
 export class UsersModule {}
