3bd55dc0ba07524bd647229b0026411e55b00382
Lapanxd <morgan.dumas@outlook.fr>
2025-06-13 11:59:14 +0200

    feat(colocation): [QUAC-37] ETQU, je veux pouvoir personnaliser l’apparence (logo, couleurs) de la colocation pour renforcer son identité

---
+++ src/app/colocations/application/use-cases/update-colocation.use-case.ts
@@ -0,0 +1,33 @@
+import { Inject, Injectable } from '@nestjs/common';
+import {
+  ColocationRepositoryGateway,
+  ColocationRepositoryToken,
+} from '../../domain/gateways/colocation.repository.gateway';
+import { UpdateColocationDto } from '../dtos/update-colocation.dto';
+import { logger } from '../../../../common/logger';
+import { ConnectedUser } from '../../../../common/types/connected-user.type';
+
+@Injectable()
+export class UpdateColocationUseCase {
+  constructor(
+    @Inject(ColocationRepositoryToken)
+    private readonly colocationRepository: ColocationRepositoryGateway
+  ) {}
+
+  async execute(
+    colocationId: number,
+    updateColocationDto: UpdateColocationDto,
+    connectedUser: ConnectedUser
+  ) {
+    const colocation = await this.colocationRepository.getById(colocationId);
+
+    colocation.title = updateColocationDto.title;
+    colocation.address = updateColocationDto.address;
+
+    logger.info(
+      `Colocation with id : ${colocationId} updated by user with id : ${connectedUser.id}`
+    );
+
+    await this.colocationRepository.save(colocation);
+  }
+}
