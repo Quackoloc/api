cea754658e6bf52e4ffd813971f2b56e926571ae
Lapanxd <morgan.dumas@outlook.fr>
2025-08-07 17:35:30 +0200

    refact(users): remove unused services & refact user service in usecase

--- src/app/user/presentation/controllers/user.controller.ts
+++ src/app/user/presentation/controllers/user.controller.ts
@@ -2,16 +2,16 @@ import { Controller, Get } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { UserDto } from '../../application/dtos/user.dto';
 import { GetConnectedUser } from '../../../../common/decorators/get-connected-user.decorator';
-import { UsersService } from '../../users.service';
 import { ConnectedUser } from '../../../../common/types/connected-user.type';
+import { GetUserUseCase } from '../../application/use-cases/get-user.use-case';
 
 @ApiTags('Users')
 @Controller('users')
 export class UserController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly getUserUseCase: GetUserUseCase) {}
 
   @Get('@me')
   async me(@GetConnectedUser() connectedUser: ConnectedUser): Promise<UserDto> {
-    return this.usersService.getOneById(connectedUser.id);
+    return this.getUserUseCase.execute(connectedUser.id);
   }
 }
